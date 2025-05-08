ece65ab11ba8d7bed199b2b5208f2f62496f90df
Amira <145941490+amirafuad@users.noreply.github.com>
2025-05-08 10:07:05 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,21 @@
 import type React from "react"
 import type { Metadata } from "next"
-
 import "./globals.css"
 
-
-export const metadata = {
+export const metadata: Metadata = {
   title: "Shef Habibi - Coming Soon",
   icons: {
     icon: "/favicon.ico",
   },
+  themeColor: "#dabd91",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="theme-color" content="#dabd91" />
-      </head>
-      <body suppressHydrationWarning={true}>{children}</body>
+      <body suppressHydrationWarning={true}>
+        {children}
+      </body>
     </html>
   )
 }
